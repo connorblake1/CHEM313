3876aad7a23106ff0a51043f1fed7a260e9ba7d8
Connor Blake <theconnorblake@gmail.com>
2025-05-23 03:12:23 -0500

    presentation figures and msm stuff

--- final_project/sc_committor_rates/2D/general.py
+++ final_project/sc_committor_rates/2D/general.py
@@ -75,10 +75,10 @@ cmask = torch.ones(max_K)
 # centers_k = torch.stack((a_center,b_center),dim=0)
 
 centers_k = torch.tensor(
-    [[-2.9428, -0.0838],
+    [[-3., 0], # a
     [-1.5254, -0.0344],
     [ 1.4761,  0.0231],
-    [ 3.0663, -0.0864]]
+    [ 3.0, 0.]] # b
 )
 ##<\CHANGE THESE PARAMETERS>
 
